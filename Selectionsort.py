8b8cfc2c8b5732ae4337eff44a94fe16a5d17ffc
Jajularamatulasi <164602865+Jajularamatulasi@users.noreply.github.com>
2024-05-30 15:17:50 +0530

    Add files via upload

---
+++ Selectionsort.py
@@ -0,0 +1,23 @@
+def PerformSelectionsort(nums):
+    n=len(nums)
+    fixThisIndex=n-1
+
+    while fixThisIndex>0:
+        maxEleIndex=fixThisIndex
+        for index in range(fixThisIndex):
+            if nums[index]>nums[maxEleIndex]:
+                maxEleIndex=index
+        if maxEleIndex!=fixThisIndex:
+            temp=nums[maxEleIndex]
+            nums[maxEleIndex]=nums[fixThisIndex]
+            nums[fixThisIndex]=temp
+            
+                
+        print(nums)
+        fixThisIndex -= 1
+                
+                
+nums=[10,8,2,14,12,7]
+print("Before sorting:",nums)
+PerformSelectionsort(nums)
+print("After sorting:",nums)
